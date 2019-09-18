fce1e1ef4c661a051a1d1cb97d0d4163f50d84a8
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-18 22:49:58 +0900

    introduce navigation

--- App.tsx
+++ App.tsx
@@ -3,13 +3,21 @@ import Main from "./Main";
 import {Provider} from "react-redux";
 import {createStore} from "redux";
 import reducer from "./NekoReducer";
+import {createStackNavigator} from "react-navigation-stack";
+import {createAppContainer} from "react-navigation";
+
+const MainNavigator = createStackNavigator({
+    Main: {screen: Main},
+});
+
+const Navigation = createAppContainer(MainNavigator);
 
 const store = createStore(reducer);
 
 export default function App() {
     return (
         <Provider store={store}>
-            <Main/>
+            <Navigation/>
         </Provider>
     );
 }
