8e2efdafc4b1bc577cceffafffc6a09f6c84223e
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-18 22:22:34 +0900

    define fetch action

---
+++ NekoAction.ts
@@ -0,0 +1,7 @@
+export const FETCH_NEKO = 'FETCH_NEKO'
+
+interface FetchNekoAction {
+    type: typeof FETCH_NEKO
+}
+
+export type NekoActionTypes = FetchNekoAction
--- NekoReducer.ts
+++ NekoReducer.ts
@@ -1,8 +1,15 @@
 import {NekoState} from "./NekoState";
+import {FETCH_NEKO, NekoActionTypes} from "./NekoAction";
 
 const initialState: NekoState = {
+    urls: []
 };
 
-export function nekoReducer(state = initialState): NekoState {
-    return state
+export function nekoReducer(state = initialState, action: NekoActionTypes): NekoState {
+    switch (action.type) {
+        case FETCH_NEKO:
+            return state;
+        default:
+            return state;
+    }
 }
