9170724822b529a13b9fcab8659d604ed3c1ec3e
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-23 08:48:36 +0900

    add repository

---
+++ NekoRepository.ts
@@ -0,0 +1,8 @@
+class NekoRepository {
+    static readonly URL = "https://api.thecatapi.com/v1/images/search?limit=";
+
+    getAll(limit: number): Promise<NekoResponse[]> {
+        return fetch(NekoRepository.URL + limit.toString())
+            .then(response => response.json())
+    }
+}
