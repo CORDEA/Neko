d9bbdcf6e693f247bc6f830ed2f5fc9ed2f1f374
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-23 09:47:35 +0900

    apply thunk

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Main from "./Main";
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducer from "./NekoReducer";
 import {createStackNavigator} from "react-navigation-stack";
 import {createAppContainer} from "react-navigation";
+import thunk from "redux-thunk";
 
 const MainNavigator = createStackNavigator({
     Main: {screen: Main},
@@ -12,7 +13,7 @@ const MainNavigator = createStackNavigator({
 
 const Navigation = createAppContainer(MainNavigator);
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default function App() {
     return (
