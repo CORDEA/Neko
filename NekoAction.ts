c274ebb08095251302d9ec8fd90c6bae9b3c27c1
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-23 09:30:13 +0900

    fetch images

--- NekoAction.ts
+++ NekoAction.ts
@@ -1,7 +1,10 @@
-export const FETCH_NEKO = 'FETCH_NEKO'
+import {NekoResponse} from "./NekoResponse";
+
+export const FETCH_NEKO = 'FETCH_NEKO';
 
 interface FetchNekoAction {
-    type: typeof FETCH_NEKO
+    type: typeof FETCH_NEKO,
+    response: NekoResponse[],
 }
 
 export type NekoActionTypes = FetchNekoAction
--- NekoActions.ts
+++ NekoActions.ts
@@ -1,7 +1,22 @@
 import {FETCH_NEKO, NekoActionTypes} from "./NekoAction";
+import {Dispatch} from "redux";
+import {ThunkAction} from "redux-thunk";
+import {NekoState} from "./NekoState";
+import NekoRepository from "./NekoRepository";
+import {NekoResponse} from "./NekoResponse";
 
-export function fetchNeko(): NekoActionTypes {
+const repository = new NekoRepository();
+
+function _fetchNeko(response: NekoResponse[]): NekoActionTypes {
     return {
         type: FETCH_NEKO,
+        response: response,
+    }
+}
+
+export function fetchNeko(): ThunkAction<void, NekoState, null, NekoActionTypes> {
+    return async (dispatch: Dispatch<NekoActionTypes>) => {
+        const response = await repository.getAll(10);
+        dispatch(_fetchNeko(response))
     }
 }
